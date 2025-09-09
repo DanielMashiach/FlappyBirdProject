b7bcd002d0a9f3ede70232d15c43b74b61442f4b
DanielMashiach <mashi55555@gmail.com>
2025-09-09 15:29:00 +0300

    Added play button and game over, added font, fixed pipe prefab to be accurate and function in gameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,58 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Bird birdPlayer;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
     private int score;
+
+    private void Awake()
+    {
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        birdPlayer.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        foreach (Pipes pipe in pipes)
+        {
+            Destroy(pipe.gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        birdPlayer.enabled = false;
+    }
+
+
+
 
     public void GameOver()
     {
-        Debug.Log("Game Over!");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
-        Debug.Log("Score: " + score);
+        scoreText.text = score.ToString();
     }
 }
